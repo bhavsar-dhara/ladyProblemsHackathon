dd19def550e5abe03c940f435501845d313c4b90
Dhara Bhavsar <dbhavsar@massmutual.com>
2016-11-20 00:54:17 -0500

    Adding Cam Permissions Manager

--- app/src/main/java/com/angelhack/ladyproblems/CamDetectActivity.java
+++ app/src/main/java/com/angelhack/ladyproblems/CamDetectActivity.java
@@ -1,10 +1,34 @@
 package com.angelhack.ladyproblems;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Process;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class CamDetectActivity extends AppCompatActivity {
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import java.util.List;
+
+public class CamDetectActivity extends AppCompatActivity implements Detector.ImageListener, CameraDetector.CameraEventListener {
+
+    private static final String TAG = CamDetectActivity.class.getSimpleName();
+    private final static int CAMERA_PERMISSIONS_REQUEST_CODE = 0;
+    private final static String[] CAMERA_PERMISSIONS_REQUEST = new String[]{Manifest.permission.CAMERA};
+    private boolean handleCameraPermissionGrant = false;
+
+    private CameraDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +37,79 @@ public class CamDetectActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        requestPermission();
+
+//        detector = new CameraDetector(this, CameraType.CAMERA_FRONT);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSIONS_REQUEST_CODE) {
+            for (String permission : permissions) {
+                if (permission.equals(Manifest.permission.CAMERA)) {
+                    // next time through onResume, handle the grant result
+                    handleCameraPermissionGrant = true;
+                    break;
+                } else {
+                    showRationale();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCameraSizeSelected(int i, int i1, Frame.ROTATE rotate) {
+
+    }
+
+    @Override
+    public void onImageResults(List<Face> list, Frame frame, float v) {
+
+    }
+
+    private void requestPermission() {
+        if (!handleCameraPermissionGrant
+                && !checkPermission()
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(CAMERA_PERMISSIONS_REQUEST, CAMERA_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private void showRationale() {
+        final AlertDialog dialog = new AlertDialog.Builder(this, R.style.CustomAlertDialogTheme)
+                .setTitle("")
+                .setMessage("This app needs camera permission to use this feature for quick emotion recognition.")
+                .setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (dialog != null) {
+                            dialog.dismiss();
+                        }
+                        requestPermission();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        userCancelled(null);
+                    }
+                })
+                .create();
+
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
+    public void userCancelled(View v) {
+        // TODO
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private boolean checkPermission() {
+        return this.checkPermission(Manifest.permission.CAMERA, Process.myPid(), Process.myUid())
+                == PackageManager.PERMISSION_GRANTED;
+    }
 }
